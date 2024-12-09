02c78706859a04e0e567d4ee370df4a6775ab778
chsaecho <163527579+Ratdawg42@users.noreply.github.com>
2024-12-09 11:14:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ password = "spsm"
 
 input_password = input("please enter your password: ")
 if password == input_password:
-    print("correct!")
+    print("Correct!")
 else:
     print("Wrong password!")
