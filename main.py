798a19e917725ddb91afc79391b38cb0d6810392
chsaecho <163527579+Ratdawg42@users.noreply.github.com>
2024-12-09 11:10:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("HelloWorld")
+password = "spsm"
+
+input_password = input("please enter your password: ")
+if password == input_password:
+    print("correct!")
+else:
+    print("Wrong password!")
